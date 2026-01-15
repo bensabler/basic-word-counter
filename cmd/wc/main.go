86ad517715998eef5cc20d3ce6e7453b26d7ec9a
Ben Sabler <ben.sabler@velocityprint.com>
2026-01-15 13:01:59 -0500

    added a flag to count bytes and a test

--- cmd/wc/main.go
+++ cmd/wc/main.go
@@ -12,22 +12,28 @@ func main() {
 	// Defining a boolean flag -l to count lines instead of words
 	lines := flag.Bool("l", false, "Count lines")
 
+	// Defining a boolean flag -b to count bytes instead of words
+	bytes := flag.Bool("b", false, "Count bytes")
+
 	// Parsing the flags provided by the user
 	flag.Parse()
 
-	// Calling the count function to count the number of words (or lines)
+	// Calling the count function to count the number of words (or lines/bytes)
 	// received from the Standard Input and printing it out
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines, countBytes bool) int {
 	// A scanner is used to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
 
-	// If the count lines flag is not set , mwe want to count words so we define
+	// If the count lines/bytes flag is not set, we want to count words so we define
 	// the scanner split type to words (default is split by lines)
-	if !countLines {
+	if !countLines && !countBytes {
 		scanner.Split(bufio.ScanWords)
+	} else if countBytes {
+		// If the count bytes flag is set, we read the entire input as a single token
+		scanner.Split(bufio.ScanBytes)
 	}
 
 	// Defining a counter
